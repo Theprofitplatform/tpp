698d1509cc625e444b0950cb0e0349dd90f4dff8
Theprofitplatform <avi@theprofitplatform.com.au>
2025-09-21 03:15:45 +1000

    updates

--- tests/setup.js
+++
@@ -1,6 +0,0 @@
-// Test setup file
-const { TextEncoder, TextDecoder } = require('util');
-
-// Polyfill TextEncoder/TextDecoder for JSDOM
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
